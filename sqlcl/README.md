9d96d956cedb66c7898f0f2836246df91c8c1487
Gerald Venzl <gerald.venzl@oracle.com>
2023-05-18 15:47:36 -0700

    Restructure bash source and use emojis
    Signed-off-by: Gerald Venzl <gerald.venzl@oracle.com>

--- sqlcl/README.md
+++ sqlcl/README.md
@@ -1,5 +1,6 @@
 # SQLcl tools
 
-| File | Description   |
-|------|---------------|
-| [sql.sh](./sql.sh) | Bash script alias for always downloading and using the latest SQLcl. |
+| File               | Description                  |
+|--------------------|------------------------------|
+| [sql](./sql)       | Bash script alias that can be sourced to always download and use the latest SQLcl. |
+| [sql.sh](./sql.sh) | Bash script for always downloading and using the latest SQLcl. |
--- sqlcl/sql.sh
+++ sqlcl/sql.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+#
 # MIT License
 #
-# Copyright (c) 2023 Kris Rice, Gerald Venzl
+# Copyright (c) 2023 Gerald Venzl
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,55 +22,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-sql() {
-  # Remote source this script
-  #
-  # . <(curl -s https://github.com/gvenzl/oracle-tools/blob/main/sqlcl.sh)
+. sql
 
-  # Set the stage directory
-  STAGE_DIR=/var/tmp
-
-  # Check whether internet connection exists
-  if ping -c 1 -W 3 download.oracle.com > /dev/null; then
-
-    # Get current ETAG from download
-    ETAG=$(curl -I -s https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip | tr -d '\r'  | sed -En 's/^ETag: (.*)/\1/p')
-
-    echo "REMOTE-ETAG: $ETAG"
-
-    # Compare to last ETag saved
-    if [[ -e $STAGE_DIR/sqlcl.etag ]]; then
-        CURRENT_ETAG=$(cat $STAGE_DIR/sqlcl.etag)
-        echo "LOCAL-ETAG: $CURRENT_ETAG"
-    else
-        CURRENT_ETAG="none"
-    fi
-
-    # Check if ETags match
-    if [[ "$ETAG" != "$CURRENT_ETAG" ]]; then
-      echo "Downloading..."
-      curl -sS -o $STAGE_DIR/sqlcl-latest.zip \
-            --header 'If-None-Match: "${CURRENT_ETAG}"' \
-            https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip
-      echo Unzip....
-      echo "$ETAG" > $STAGE_DIR/sqlcl.etag
-      echo "Removing old SQLcl"
-      rm -rf $STAGE_DIR/sqlcl
-      echo Unzipping to $STAGE_DIR/sqlcl
-      unzip   -qq -d $STAGE_DIR $STAGE_DIR/sqlcl-latest.zip
-    else
-      echo "SQLcl is current"
-    fi
-   else
-     echo "Internet connection to download.oracle.com not available."
-     if [[ ! -f $STAGE_DIR/sqlcl/bin/sql ]]; then
-       echo "SQLcl cannot be downloaded, please check internet connection."
-       return 1;
-     else
-       echo "Cannot verify latest SQLcl version."
-     fi
-   fi
-
-   # Run SQLcl
-   $STAGE_DIR/sqlcl/bin/sql "$*"
-}
+sql "$@"
